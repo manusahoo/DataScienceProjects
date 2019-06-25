8fedafecdcfe4190d1df5ffac79711712cc71961
Manoranjan Sahoo <manusahoo@gmail.com>
2019-06-25 16:37:32 -0400

    adding housing value project

--- IrisClassification/iris.py
+++ IrisClassification/iris.py
@@ -12,6 +12,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestRegressor
 
 filepath = '/Users/manoranjansahoo/Documents/iris.csv'
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
@@ -19,17 +20,19 @@ irisdataset = pandas.read_csv(filepath, names=names)
 
 # Split-out validation dataset
 array = irisdataset.values
-X = array[:,0:4]
-Y = array[:,4]
+X = array[:, 0:4]
+Y = array[:, 4]
 validation_size = 0.20
 seed = 7
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
+    X, Y, test_size=validation_size, random_state=seed)
 
 scoring = 'accuracy'
 
 # Spot Check Algorithms
 models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LR', LogisticRegression(
+    solver='liblinear', multi_class='ovr')))
 models.append(('LDA', LinearDiscriminantAnalysis()))
 models.append(('KNN', KNeighborsClassifier()))
 models.append(('CART', DecisionTreeClassifier()))
@@ -39,13 +42,15 @@ models.append(('SVM', SVC(gamma='auto')))
 results = []
 names = []
 for name, model in models:
-	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-	results.append(cv_results)
-	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(
+        model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
 
+RandomForestRegressor()
 # Make predictions on validation dataset
 knn = KNeighborsClassifier()
 knn.fit(X_train, Y_train)
